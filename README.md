5a789ac83ebde58ffa3c085bc556459b5e8a5047
pnot0 <diogonucci09092005@gmail.com>
2025-04-24 22:29:13 -0300

    project start

---
+++ README.md
@@ -0,0 +1,5 @@
+API REST basica projeto Argotech
+
+usando
+- express
+- body parser
---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.listen(8080, () => {
+    console.log("iniciado");
+})
