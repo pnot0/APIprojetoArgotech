106f98307badf173fe5260ebb615919666b8a266
pnot0 <diogonucci09092005@gmail.com>
2025-04-24 22:49:46 -0300

    database connected

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ API REST basica projeto Argotech
 usando
 - express
 - body parser
+- sequelize
+- mysql2
--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const con = require("./database/database");
+
+//Autenticação do banco de dados
+//Try catch com conexão, retorna mensagem de erro caso falhe
+con.authenticate()
+.then(() => {
+    console.log("connection success");
+}).catch((errorMsg) => {
+    console.log(errorMsg);
+});
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
+//Abrindo porta do servidor 8080
 app.listen(8080, () => {
     console.log("iniciado");
 })
