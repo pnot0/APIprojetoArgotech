ae8bdf636ab48f7355cbc08014a6e18246ac5d7c
pnot0 <diogonucci09092005@gmail.com>
2025-04-29 17:49:20 -0300

    added all routes

--- database/database.js
+++ database/database.js
@@ -3,5 +3,4 @@ const sequelize = require("sequelize");
 //Criando conexão do banco e exportando ela
 //colocar configurações do banco aqui que estão no chat #dev
 
-
 module.exports = con;
--- index.js
+++ index.js
@@ -17,9 +17,6 @@ con.authenticate()
     console.log(errorMsg);
 });
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-
 //Pegar todos elementos de uma tabela
 function getAll(model, res){
     model.findAll().then(x => {res.status(200).json(x)})
@@ -28,29 +25,56 @@ function getAll(model, res){
 
 //Pegar um elemento de uma tabela com base no ID
 function getId(model, res, searchId){
-    //Pegar sempre o nome de um ID em uma tabela 
-    const primaryKey = model.primaryKeyAttributes[0];
-    model.findOne({
-        where: {[primaryKey]: searchId}
-    }).then(x => {res.status(200).json(x)})
-    .catch(error => {res.status(500).json({ error: error.message})});
+    //Se searchId não for um número retornar 400
+    if(isNaN(searchId)){res.status(400).send()}
+    else{ 
+        //Pegar sempre o nome de um ID em uma tabela 
+        const primaryKey = model.primaryKeyAttributes[0];
+        model.findOne({
+            where: {[primaryKey]: searchId}
+        }).then(x => {
+            //Se achar x mande 200 e seu conteudo
+            if(x!=undefined){
+                res.status(200).json(x);
+            //Senão mande 404
+            }else{
+                res.status(404).send();
+            }}).catch(error => {res.status(500).json({ error: error.message})});
+    }
 }
 
-app.get("/clientes",(req, res) => {
-    getAll(Clientes, res);
-});
+function createGetAllRoute(route, model){
+    app.get("/"+route,(req, res) => {
+        getAll(model, res);
+    });
+}
 
-app.get("/componentes",(req, res) => {
-    getAll(Componentes, res);
-})
+function createGetIdRoute(route, model){
+    app.get("/"+route+"/:id",(req, res) => {
+        getId(model, res, req.params.id);
+    });
+}
 
-app.get("/clientes/:id",(req, res) => {
-    getId(Clientes, res, req.params.id);
-})
+routes = [
+    "clientes",
+    "componentes",
+    "computadores",
+    "estoque",
+    "vendas"
+]
 
-app.get("/componentes/:id",(req, res) => {
-    getId(Componentes, res, req.params.id);
-})
+models = [
+    Clientes,
+    Componentes,
+    Computadores,
+    Estoque,
+    Vendas
+]
+
+for(i=0;i<routes.length;i++){
+    createGetAllRoute(routes[i], models[i]);
+    createGetIdRoute(routes[i], models[i]);
+}
 
 //Abrindo porta do servidor 8080
 app.listen(8080, () => {
